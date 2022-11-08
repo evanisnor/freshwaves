af192eb66b60426ddcddf50a51b5c3ca635eb669
Evan Isnor <ewisnor@gmail.com>
2022-11-07 22:16:54 -0400

    Update deprecated getApplicationInfo call for Tiramisu

--- app/src/main/java/com/evanisnor/freshwaves/system/AppMetadata.kt
+++ app/src/main/java/com/evanisnor/freshwaves/system/AppMetadata.kt
@@ -2,6 +2,7 @@ package com.evanisnor.freshwaves.system
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import javax.inject.Inject
 
 class AppMetadata @Inject constructor() {
@@ -10,8 +11,18 @@ class AppMetadata @Inject constructor() {
 
     fun spotifyRedirectUri(context: Context) = metaData(context, "spotifyRedirectUri")
 
-    private fun metaData(context: Context, id: String) = context.packageManager.getApplicationInfo(
-        context.packageName,
-        PackageManager.GET_META_DATA
-    ).metaData[id].toString()
+    private fun metaData(context: Context, id: String) =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            context.packageManager.getApplicationInfo(
+                context.packageName,
+                PackageManager.ApplicationInfoFlags.of(PackageManager.GET_META_DATA.toLong())
+            )
+        } else {
+            @Suppress("DEPRECATION")
+            context.packageManager.getApplicationInfo(
+                context.packageName,
+                PackageManager.GET_META_DATA
+            )
+        }.metaData.getString(id)
+            ?: throw IllegalStateException("Unable to read app metadata")
 }

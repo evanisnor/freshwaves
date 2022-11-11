b2e90603b97182b77ec2820cc5dfbe526455373a
Evan Isnor <ewisnor@gmail.com>
2022-11-10 20:56:05 -0400

    Adding FakeSpotifyAPIService

---
+++ app/src/test/java/com/evanisnor/freshwaves/spotify/FakeSpotifyAPIService.kt
@@ -0,0 +1,35 @@
+package com.evanisnor.freshwaves.spotify
+
+import com.evanisnor.freshwaves.spotify.network.SpotifyAPIService
+import com.evanisnor.freshwaves.spotify.network.model.*
+
+class FakeSpotifyAPIService : SpotifyAPIService {
+
+    var privateUser: PrivateUserObject? = null
+    var topArtists: List<ArtistObject>? = null
+    var artistAlbums: List<AlbumObject>? = null
+    var albumTracks: List<TrackObject>? = null
+
+    override suspend fun getUserProfile(accessToken: String): PrivateUserObject =
+        requireNotNull(privateUser)
+
+    override suspend fun getTopArtists(
+        accessToken: String,
+        limit: Int,
+        offset: Int
+    ): PagingObject<ArtistObject> =
+        PagingObject(requireNotNull(topArtists).slice(offset until offset + limit))
+
+    override suspend fun getArtistAlbums(
+        accessToken: String,
+        artistId: String,
+        market: String,
+        limit: Int,
+        includeGroups: String
+    ): PagingObject<AlbumObject> = PagingObject(requireNotNull(artistAlbums))
+
+    override suspend fun getAlbumTracks(
+        accessToken: String,
+        albumId: String
+    ): PagingObject<TrackObject> = PagingObject(requireNotNull(albumTracks))
+}

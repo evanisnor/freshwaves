1a5f241ff966e97922ce4f8244f9d5c37645d3c0
Evan Isnor <ewisnor@gmail.com>
2022-11-12 10:57:33 -0400

    Formatting

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,26 @@
 rootProject.name = "Fresh Waves"
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-    versionCatalogs {
-        create("libs")
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+  versionCatalogs {
+    create("libs")
+  }
 }
 
 pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-    }
+  repositories {
+    gradlePluginPortal()
+    google()
+  }
 }
 
 include(":app")
 includeBuild("submodules/handyauth") {
-    dependencySubstitution {
-        substitute(module("com.evanisnor.handyauth:client")).using(project(":client"))
-    }
+  dependencySubstitution {
+    substitute(module("com.evanisnor.handyauth:client")).using(project(":client"))
+  }
 }

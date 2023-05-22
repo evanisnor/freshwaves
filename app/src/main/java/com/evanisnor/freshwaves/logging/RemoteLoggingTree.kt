7cc904ce81c47865573beabf74d5ea333f719a28
Evan Isnor <ewisnor@gmail.com>
2023-05-22 11:13:24 -0300

    Update remote logging to include tag, exceptions, and info lines

--- app/src/main/java/com/evanisnor/freshwaves/logging/RemoteLoggingTree.kt
+++ app/src/main/java/com/evanisnor/freshwaves/logging/RemoteLoggingTree.kt
@@ -12,8 +12,13 @@ class RemoteLoggingTree : Timber.Tree() {
 
   override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
     when (priority) {
-      Log.ERROR -> Firebase.crashlytics.log("ERROR: $message")
-      Log.WARN -> Firebase.crashlytics.log("WARN: $message")
+      Log.ERROR -> Firebase.crashlytics.log("ERROR: $tag$message")
+      Log.WARN -> Firebase.crashlytics.log("WARN: $tag$message")
+      Log.INFO -> Firebase.crashlytics.log("INFO: $tag$message")
+    }
+
+    if (t != null) {
+      Firebase.crashlytics.recordException(t)
     }
   }
 }
